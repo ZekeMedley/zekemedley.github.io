edeebc8d9f45d221ba179a4148d4d772c4d58478
Zeke Medley <zekemedley@gmail.com>
2020-02-28 19:51:59 -0800

    use mistletoe renderer

---
+++ zekedoc/zekedoc.py
@@ -0,0 +1,29 @@
+import mistletoe
+import sys
+from renderer import ZekeRenderer
+
+header = '''
+<!DOCTYPE html>
+<head>
+<meta charset='UTF-8'>
+<title>Zeke</title>
+<link rel="stylesheet" href="styles/type.css"></link>
+<link rel="stylesheet" href="styles/position.css"></link>
+<!-- Favicon is GitHub profile image. -->
+<link rel="icon" href="https://avatars1.githubusercontent.com/u/30676292?s=180&v=4">
+</head>
+<body>
+<div class="col">
+'''
+
+footer = '''
+<br>
+<br>
+</body>
+</html>
+'''
+
+def render_file(f):
+    return mistletoe.markdown(sys.stdin, ZekeRenderer)
+
+
